6e461f36efe58b1aca81efcd4df792ed1923228c
kokinedo <kokinedo@gmail.com>
2025-02-23 17:28:21 -0500

    Add Vite configuration files

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Chart as ChartJS, ArcElement, RadialLinearScale, PointElement, LineElement, Tooltip, Legend } from 'chart.js';
-import './index.css'
-import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './styles/index.css'
 
-ChartJS.register(
-  ArcElement,
-  RadialLinearScale,
-  PointElement,
-  LineElement,
-  Tooltip,
-  Legend
-);
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 ) 
